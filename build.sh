eb3edc28acebcaf56a97980d0aa3b2f4e6c12ad1
pexcn <i@pexcn.me>
2019-03-11 17:32:16 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash -e
 
-> /etc/apt/sources.list
-echo "deb http://cdn-fastly.deb.debian.org/debian stretch main contrib non-free" >> /etc/apt/sources.list
-echo "deb http://cdn-fastly.deb.debian.org/debian stretch-updates main contrib non-free" >> /etc/apt/sources.list
-echo "deb http://cdn-fastly.deb.debian.org/debian stretch-backports main contrib non-free" >> /etc/apt/sources.list
-echo "deb-src http://cdn-fastly.deb.debian.org/debian stretch main contrib non-free" >> /etc/apt/sources.list
-echo "deb-src http://cdn-fastly.deb.debian.org/debian stretch-updates main contrib non-free" >> /etc/apt/sources.list
-echo "deb-src http://cdn-fastly.deb.debian.org/debian stretch-backports main contrib non-free" >> /etc/apt/sources.list
-echo "deb http://cdn-fastly.deb.debian.org/debian-security stretch/updates main contrib non-free" >> /etc/apt/sources.list
-echo "deb-src http://cdn-fastly.deb.debian.org/debian-security stretch/updates main contrib non-free" >> /etc/apt/sources.list
-
-apt-get update
-apt-get -t stretch-backports install -y build-essential libncurses5-dev lsb-release quilt kernel-wedge
-apt-get build-dep linux
-
 curl -kLs https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.105.tar.gz | tar zxf -
 
 cd linux-4.14.105
@@ -21,10 +7,10 @@ cp ../config/config-4.14.105 .config
 patch -p1 < ../config/0001-prepare-for-tcp-bbr-plus.patch
 scripts/config --disable MODULE_SIG
 scripts/config --disable DEBUG_INFO
-make deb-pkg
+make -j`nproc` deb-pkg
 cd ..
 
-mkdir deb
-cp linux-headers*.deb deb/
-cp linux-image*.deb deb/
-cp linux-libc-dev*.deb deb/
+mkdir dist
+cp linux-headers*.deb dist/
+cp linux-image*.deb dist/
+cp linux-libc-dev*.deb dist/
