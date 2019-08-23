145fff21cf6249b943afbe4acee09e4490890a06
pexcn <i@pexcn.me>
2019-08-23 22:46:44 +0800

    refine code

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
+KERNEL_LEVEL="$(echo $KERNEL_VERSION | cut -d '.' -f 1-2).x"
+
 # download sources
 curl -sSL https://cdn.kernel.org/pub/linux/kernel/v${KERNEL_VERSION:0:1}.x/linux-$KERNEL_VERSION.tar.gz | tar -zxf -
 
 # prepare config & apply patch
-cp config/config-$KERNEL_VERSION linux-$KERNEL_VERSION/.config
-if [ -d "patches/$KERNEL_VERSION" ]; then
-  find patches/$KERNEL_VERSION -type f -name "*.patch" | sort -n | xargs cat | patch -p1 -d linux-$KERNEL_VERSION
+cp config/config-$KERNEL_LEVEL linux-$KERNEL_VERSION/.config
+if [ -d "patches/$KERNEL_LEVEL" ]; then
+  find patches/$KERNEL_LEVEL -type f -name "*.patch" | sort -n | xargs cat | patch -p1 -d linux-$KERNEL_VERSION
 fi
 
 # build kernel
